9eee5c1d23ecd21e83036c4af73c17d26462da99
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-11-02 10:36:28 +0200

    search added

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,42 +3,41 @@
 @section('content')
     <h1 class="text-2xl mb-10">Books</h1>
 
-    <form>
+    <form method="GET" action="{{ route('books.index') }}" class="flex space-x-2 items-center mb-4">
+        <input type="text" name="title" class="input h-10" placeholder="Search by title" value="{{ request('title') }} ">
+        <button type="submit" class="btn h-10">Search</button>
+        <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
     </form>
 
     <ul>
         @forelse($books as $book)
-        {{-- BOOK COMPONENT --}}
-        <li class="mb-4">
-            <div class="book-item">
-              <div
-                class="flex flex-wrap items-center justify-between">
-                <div class="w-full flex-grow sm:w-auto">
-                  <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
-                  <span class="book-author">by {{ $book->author }}</span>
+            {{-- BOOK COMPONENT --}}
+            <li class="mb-4">
+                <div class="book-item">
+                    <div class="flex flex-wrap items-center justify-between">
+                        <div class="w-full flex-grow sm:w-auto">
+                            <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
+                            <span class="book-author">by {{ $book->author }}</span>
+                        </div>
+                        <div>
+                            <div class="book-rating">
+                                {{ number_format($book->reviews_avg_rating, 1) }}
+                            </div>
+                            <div class="book-review-count">
+                                out of {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                  <div class="book-rating">
-                    {{ number_format($book->reviews_avg_rating, 1) }}
-                  </div>
-                  <div class="book-review-count">
-                    out of {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
+            </li>
         @empty
-        {{-- EMPTY COMPONENT --}}
-        <li class="mb-4">
-            <div class="empty-book-item">
-              <p class="empty-text">No books found</p>
-              <a href="{{ route('books.index') }}" class="reset-link">Reset criteria</a>
-            </div>
-          </li>
-          @endforelse
+            {{-- EMPTY COMPONENT --}}
+            <li class="mb-4">
+                <div class="empty-book-item">
+                    <p class="empty-text">No books found</p>
+                    <a href="{{ route('books.index') }}" class="reset-link">Reset criteria</a>
+                </div>
+            </li>
+        @endforelse
     </ul>
-
-    
-
 @endsection
